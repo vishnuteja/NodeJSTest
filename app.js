991e04eae574ffa9c8d0e8734ab81bcad9be60ae
VishnuTeja T <vishnuteja@users.noreply.github.com>
2016-05-15 20:03:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -59,4 +59,11 @@ app.use(function(err, req, res, next) {
 });
 
 
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(3000, 'localhost');
+console.log('Server running at http://127.0.0.1:3000/');
+
 module.exports = app;
